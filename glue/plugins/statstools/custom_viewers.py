28e642dcd5a708922f243cec6360e65863ac77f9
will <judowill@gmail.com>
2015-06-06 08:24:44 -0700

    Now has a working viewer for seeing the effect sizes of paramerters in the model

--- glue/plugins/statstools/custom_viewers.py
+++ glue/plugins/statstools/custom_viewers.py
@@ -1,17 +1,46 @@
 from ... import custom_viewer
-from ...qt.custom_viewer import FormElement
+from ...qt.custom_viewer import ComponenentElement
+from data import StatsModelsComponent
 import numpy as np
 
+class StatsModelComponenentElement(ComponenentElement):
+
+    def _list_components(self):
+
+        comps = []
+        for layer in self.container.layers:
+            for c in layer.data.components:
+                comp = layer.data.get_component(c)
+                if not c._hidden and isinstance(comp, StatsModelsComponent):
+                    comps.append(c)
+        comps = sorted(comps, key=lambda x: x.label)
+        return comps
+
+    @classmethod
+    def recognizes(cls, params):
+        return params == 'StatsComp'
+
+
 effect_size_viewer = custom_viewer('Effect Sizes',
-                                   abslope = 'float',
-                                   slider = [0, 3])
+                                   prediction='StatsComp',
+                                   confidence_interval=0.95,
+                                   alpha=0.05)
 
 @effect_size_viewer.plot_data
-def plot_data(axes, abslope):
+def plot_data(axes, prediction, alpha, confidence_interval):
+    if prediction._component:
+        res = prediction._component._result
+        ci = res.conf_int(confidence_interval)
 
-    x = np.linspace(0, 40)
-    if abslope is None:
-        abslope = 1.0
-    y = abslope*x
+        width = ci[1]-ci[0]
+        alphas = np.ones_like(width.values)
+        alphas[res.pvalues < alpha] = 0.3
 
-    axes.plot(x, y)
+        axes.barh(left=ci[0],
+                  width=width,
+                  bottom=np.arange(len(width))-0.4,
+                  height=0.8,
+                  alpha=alphas)
+        axes.set_yticks(range(len(ci)))
+        axes.set_yticklabels(ci.index)
+        res.params.plot(kind='barh', ax=axes, grid=False)
